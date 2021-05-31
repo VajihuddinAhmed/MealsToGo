bf68f3996cf546787d2a075b25bc72761fe0d06e
VajihuddinAhmed <vajihahmed@gmail.com>
2021-05-31 02:11:21 +0200

    added firebase into application for authentication and also added account feature with three screens like account.screen.js, login.screen.js and register.screen.js with navigation and stylings

--- App.js
+++ App.js
@@ -11,6 +11,21 @@ import { RestaurantsContextProvider } from "./src/services/restaurants/restauran
 import { LocationContextProvider } from "./src/services/location/location.context";
 import { FavouritesContextProvider } from "./src/services/favourites/favourites.context";
 import { Navigation } from "./src/infrastructure/navigation";
+import * as firebase from "firebase";
+import { AuthenticationContextProvider } from "./src/services/authentication/authentication.context";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCqInWhzkePTCzGdF0bwRIqm0Fu3QvNeYc",
+  authDomain: "mealstogo-15553.firebaseapp.com",
+  projectId: "mealstogo-15553",
+  storageBucket: "mealstogo-15553.appspot.com",
+  messagingSenderId: "743747489794",
+  appId: "1:743747489794:web:0972e8fe326f145a08571b",
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 export default function App() {
   const [OswaldLoaded] = useOswald({
@@ -28,13 +43,15 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <FavouritesContextProvider>
-          <LocationContextProvider>
-            <RestaurantsContextProvider>
-              <Navigation />
-            </RestaurantsContextProvider>
-          </LocationContextProvider>
-        </FavouritesContextProvider>
+        <AuthenticationContextProvider>
+          <FavouritesContextProvider>
+            <LocationContextProvider>
+              <RestaurantsContextProvider>
+                <Navigation />
+              </RestaurantsContextProvider>
+            </LocationContextProvider>
+          </FavouritesContextProvider>
+        </AuthenticationContextProvider>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
